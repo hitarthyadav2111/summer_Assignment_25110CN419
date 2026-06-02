e2fcf37cc60cbd3bc3b5cc34479436a07dd1415b
Hitarth <hitarthyadav2111@gmail.com>
2026-06-02 20:28:01 +0530

    Day 6 Problem 21

---
+++ Day 6/To_Convert_Decimal_to_Binary.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class To_Convert_Decimal_to_Binary {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the decimal number");
+        int d=in.nextInt();
+        int d1=d;
+        String b="",b1="";
+        while(d!=0) {
+            b1=b1+d%2;
+            d=d/2;
+        }
+        int l=b1.length();
+        for(int i=l-1;i>=0;i--) {
+            b=b+b1.charAt(i);
+        }
+        System.out.println("Binary equivalent of "+d1+" is "+b);
+
+    }
+}
+
