33291e2b6128157a26c28fdb18a01d7000298f85
Hitarth <hitarthyadav2111@gmail.com>
2026-06-03 23:25:30 +0530

    Day 9 Problem 33

---
+++ Week 2/Day 9/To_Print_Reverse_Star_Pattern.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class To_Print_Reverse_Star_Pattern {
+    public static void main(String args[]) {
+        for(int i=5;i>=1;i--) {
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+
+            }
+            System.out.println();
+
+        }
+    }
+    
+}
