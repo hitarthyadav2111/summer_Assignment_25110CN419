16500bb149648fc0c419e80326b9c886971b6b92
Hitarth <hitarthyadav2111@gmail.com>
2026-06-03 23:09:49 +0530

    Day 8 Problem 31

---
+++ Week 2/Day 8/To_Print_Character_Triangle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class To_Print_Character_Triangle {
+    public static void main(String args[]) {
+        for(int i=1;i<=5;i++) {
+            for(int j=1;j<=i;j++) {
+                System.out.print((char)('A'+j-1));
+            }
+            System.out.println();
+        }
+    }
+    
+}
