a13371f9cc4ce3b6afb9fe0dc6fd595705027f4f
Hitarth <hitarthyadav2111@gmail.com>
2026-06-08 22:45:57 +0530

    Day 10 Problem 39

---
+++ Week 2/Day 10/To_Print_Number_Pyramid.java
@@ -0,0 +1,16 @@
+public class To_Print_Number_Pyramid {
+    public static void main(String args[]) {
+        for(int i=1;i<=5;i++) {
+            for(int j=1;j<=5-i;j++) {
+                System.out.print("  ");
+            }
+            for(int k=1;k<=i;k++) {
+                System.out.print(k+" ");
+            }
+            for(int l=1;l<=i-1;l++) {
+                System.out.print(i-l+" ");
+            }
+            System.out.println();
+        }
+    }
+}
