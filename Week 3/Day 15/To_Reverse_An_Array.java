346065eea7ff8b06120e3c29c783e0e910f96274
Hitarth <hitarthyadav2111@gmail.com>
2026-06-11 16:51:28 +0530

    Day 15 Problem 57

---
+++ Week 3/Day 15/To_Reverse_An_Array.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class To_Reverse_An_Array {
+    public static void main(String args[]){
+        Scanner in= new Scanner(System.in);
+        System.out.println("Enter the size of the array ");
+        int n= in.nextInt();
+        int a[]=new int[n];
+        System.out.println("Enter the terms in the array");
+        for(int i=0;i<n;i++) {
+            a[i]=in.nextInt();
+        }
+        System.out.println("The array entered is");
+        for(int i=0;i<n;i++) {
+            if(i!=n-1)
+            System.out.print(a[i]+",");
+            else
+            System.out.println(a[i]);
+        }
+        int b[]=new int[n];
+        for(int i=0;i<n;i++){
+            b[n-1-i]=a[i];
+        }
+        System.out.println("The reversed array is");
+        for(int i=0;i<n;i++) {
+            if(i!=n-1)
+            System.out.print(b[i]+",");
+            else
+            System.out.println(b[i]);
+        }
+}
+}
