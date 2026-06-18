c65ce87284b6fb72c5254df78ea89f051512c00a
Hitarth <hitarthyadav2111@gmail.com>
2026-06-19 01:20:31 +0530

    Day 19 Problem 73

---
+++ Week 4/Day 19/Program_To_Add_Matrices.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class Program_To_Add_Matrices {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number of rows of matrices");
+        int r=in.nextInt();
+        System.out.println("Enter the number of coulumns of matrices");
+        int c=in.nextInt();
+        System.out.println("Enter the values in the first matrix");
+        int a[][]=new int[r][c];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                a[i][j]=in.nextInt();
+            }
+        }
+        System.out.println("The first matrix is ");
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                System.out.print(a[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("Enter the values in the second matrix");
+        int b[][]=new int[r][c];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                b[i][j]=in.nextInt();
+            }
+        }
+        System.out.println("The second matrix is ");
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                System.out.print(b[i][j]+" ");
+            }
+            System.out.println();
+        }
+        int s[][]=new int[r][c];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                s[i][j]=a[i][j]+b[i][j];
+            }
+        }
+        System.out.println("The sum of the two matrices is ");
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                System.out.print(s[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
