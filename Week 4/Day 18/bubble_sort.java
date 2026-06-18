2018a640140e03495412735980c00c1463d9d0a7
Hitarth <hitarthyadav2111@gmail.com>
2026-06-18 20:00:12 +0530

    Day 18 Problem 69

---
+++ Week 4/Day 18/bubble_sort.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class bubble_sort {
+    public static void main(String args[]){
+        Scanner in= new Scanner(System.in);
+        System.out.println("Enter the size of the array ");
+        int n= in.nextInt();
+        int a[]=new int[n];
+        System.out.println("Enter the terms in the array");
+        for(int i=0;i<n;i++) {
+            a[i]=in.nextInt();
+        }
+        System.out.println("The array entered is");
+        for(int i=0;i<n;i++) {
+            if(i!=n-1)
+            System.out.print(a[i]+",");
+            else
+            System.out.println(a[i]);
+        }
+        int temp=0;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(a[j]>a[j+1]){
+                    temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                }
+            }
+        }
+        System.out.println("The sorted array is ");
+        for(int i=0;i<n;i++) {
+            if(i!=n-1)
+            System.out.print(a[i]+",");
+            else
+            System.out.println(a[i]);
+        }
+}
+}
