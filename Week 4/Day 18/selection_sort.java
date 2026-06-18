47c371eb6305afeee2b4d256bf2190fe5f371321
Hitarth <hitarthyadav2111@gmail.com>
2026-06-18 20:15:16 +0530

    Day 18 Problem 70

---
+++ Week 4/Day 18/selection_sort.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class selection_sort {
+    public static void main(String args[]){
+        Scanner in= new Scanner(System.in);
+        System.out.println("Enter the size of the array ");
+        int n= in.nextInt();
+        int a[]=new int[n];
+        System.out.println("Enter the terms in the array");
+        for(int i=0;i<n;i++) {
+            a[i]=in.nextInt();
+        }
+        System.out.println("The array entered is");
+        for(int i=0;i<n;i++) {
+            if(i!=n-1)
+            System.out.print(a[i]+",");
+            else
+            System.out.println(a[i]);
+        }
+        int temp=0;;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(a[j]<a[i]){
+                    temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                }
+            }
+        }
+        System.out.println("The sorted array is ");
+        for(int i=0;i<n;i++) {
+            if(i!=n-1)
+            System.out.print(a[i]+",");
+            else
+            System.out.println(a[i]);
+        }
+}
+}
