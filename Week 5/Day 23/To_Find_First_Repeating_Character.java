b30343bcb33b59c6d0c78ee80e4be1b358ace7b3
Hitarth <hitarthyadav2111@gmail.com>
2026-06-23 18:58:46 +0530

    Day 23 Problem 90

---
+++ Week 5/Day 23/To_Find_First_Repeating_Character.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class To_Find_First_Repeating_Character {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the string");
+        String str=in.nextLine();
+        int l = str.length();
+        char ch=' ';
+        int count=0;
+        for(int i=0;i<l;i++){
+            ch=str.charAt(i);
+            if(ch==' ')
+                continue;
+            for(int j=0;j<l;j++){
+                if(ch==str.charAt(j)){
+                    count++;
+                }
+            }
+            if(count>1){
+                System.out.println(ch+" is the first repeating character");
+                break;
+            }
+            count=0;
+        }
+    }
+}
